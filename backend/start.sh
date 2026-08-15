7e089b89efbc772e7c7de5b6f28efaf215ead56d
alejandro <alejandro@debian.alejandro>
2026-08-15 09:27:07 -0500

    quitar entorno virtual

--- backend/start.sh
+++ backend/start.sh
@@ -2,7 +2,7 @@
 set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$SCRIPT_DIR/venv/bin/activate"
+#source "$SCRIPT_DIR/venv/bin/activate"
 export DOTENV_PATH="$SCRIPT_DIR/.env"
 
 # ─────────────────────────────────────────────────────────────────────────────
